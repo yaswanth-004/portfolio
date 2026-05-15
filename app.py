9e9719738979449658dd0db7273adf718dd44052
yaswanth-004 <2004yaswanth@gmail.com>
2026-05-15 15:26:02 +0530

    Initial portfolio upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/achievements')
+def achievements():
+    return render_template('achievements.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
